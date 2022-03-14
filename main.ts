7cc232ce9ce1594e5f5af96e411c118c0e35a6ab
pacobaor <98743296+pacobaor@users.noreply.github.com>
2022-03-14 11:57:41 +0100

    yihdgfyhrf

--- main.ts
+++ main.ts
@@ -1,25 +1,24 @@
 function girar90derecha () {
     maqueen.motorStop(maqueen.Motors.All)
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 100)
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
-    basic.pause(260)
+    basic.pause(200)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 20)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
+    basic.pause(800)
     maqueen.motorStop(maqueen.Motors.All)
 }
 function girar90izquierda () {
     maqueen.motorStop(maqueen.Motors.All)
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 100)
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 100)
-    basic.pause(260)
+    basic.pause(200)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 20)
+    basic.pause(800)
     maqueen.motorStop(maqueen.Motors.All)
 }
 basic.forever(function () {
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) == 15) {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) == 4) {
         maqueen.motorStop(maqueen.Motors.All)
         girar90izquierda()
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
-        basic.pause(260)
-        girar90derecha()
     } else {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
     }
 })

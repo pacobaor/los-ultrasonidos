8e3274bf9f7cdfd9132c75b3a78807ee805de43b
pacobaor <98743296+pacobaor@users.noreply.github.com>
2022-03-16 09:00:16 +0100

    Paco Baor y Diegucci

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 function girar90derecha () {
     maqueen.motorStop(maqueen.Motors.All)
-    basic.pause(200)
+    basic.pause(220)
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 20)
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
     basic.pause(800)
@@ -8,16 +8,23 @@ function girar90derecha () {
 }
 function girar90izquierda () {
     maqueen.motorStop(maqueen.Motors.All)
-    basic.pause(200)
+    basic.pause(220)
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 20)
     basic.pause(800)
     maqueen.motorStop(maqueen.Motors.All)
 }
+let ultimogiro = 0
 basic.forever(function () {
     if (maqueen.Ultrasonic(PingUnit.Centimeters) == 4) {
         maqueen.motorStop(maqueen.Motors.All)
-        girar90izquierda()
+        if (ultimogiro == 0) {
+            girar90izquierda()
+            ultimogiro = 1
+        } else {
+            girar90derecha()
+            ultimogiro = 0
+        }
     } else {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
     }
